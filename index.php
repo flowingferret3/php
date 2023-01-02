4583c9c5abb81c644de6286737fb41c2f938f602
Ferret <43520027+flowingferret3@users.noreply.github.com>
2023-01-02 19:56:10 +0000

    start

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Showcase</title>
+</head>
+<body>
+    <form action="POST">
+        <input type="number" name="num1" id="" value="<?php
+            $number1 = $_GET("num1");
+            $number2 = $_GET("num2");
+        ?>">
+    </form>
+    
+    <?php
+
+        
+    ?>
+</body>
+</html>
